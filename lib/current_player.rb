ab435974ba5c5d4f3adee204ecb5791884c92c2c
Scott Hall <scotthall127@gmail.com>
2018-05-28 02:13:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,19 @@
 def turn_count(board)
+<<<<<<< HEAD
   counter = 0
   board.each do |space|
     if space == "X" || space == "O"
+=======
+  board.each do |space|
+    counter = 0
+    if space == '"X"' || space == '"O"'
+>>>>>>> 3f0fa718b4f32ee5e5479d534b3fb052b37081ae
       counter += 1
     end
   end
   counter
 end
+<<<<<<< HEAD
 
 def current_player(board)
   player = "X"
@@ -15,3 +22,5 @@ def current_player(board)
   end
   player
 end
+=======
+>>>>>>> 3f0fa718b4f32ee5e5479d534b3fb052b37081ae
